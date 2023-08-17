90261e83b05b87deef04d2bd129b8d01c1d34a47
ek1903 <elene-ka@mail.ru>
2023-08-17 05:03:02 +0400

    Update task2.java

--- task2.java
+++ task2.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 public class task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        if (name.equals("Вячеслав")) {
-            System.out.println("Привет, Вячеслав");
+        try {
+            String name = scanner.nextLine();
+            if (name.equals("Вячеслав")) {
+                System.out.println("Привет, Вячеслав");
+            }
+            else {
+                System.out.println("Нет такого имени");
+            }
         }
-        else {
-            System.out.println("Нет такого имени");
+        catch (Exception e) {
+            System.out.println("Something went wrong.");
         }
     }
 }
